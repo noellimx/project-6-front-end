2ef763b1efd2ae9837e01525ee77fafcce5b9cf1
Noel Lim <noellimx@gmail.com>
2022-06-12 21:04:17 +0800

    resolve dependencies

--- client/index.tsx
+++ client/index.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 
-
 import { MAny } from "./utils/my-types";
 import SignIn from "./components/Signin";
-
 
 // UI Injection
 const rootHTMLElement: HTMLElement = document.createElement("div");
@@ -14,4 +12,10 @@ const PAINT = (htmlEle: HTMLElement, rEle: MAny) => {
   createRoot(htmlEle).render(rEle);
 };
 
-PAINT(rootHTMLElement, <div> <SignIn /> </div>);
+PAINT(
+  rootHTMLElement,
+  <div>
+    {" "}
+    <SignIn />{" "}
+  </div>
+);
