035c327a66dbba13637ea6258be7aca40b02978b
Deetzyzb <d.shawnyeo@gmail.com>
2022-06-16 22:15:25 +0800

    Added charts components, working on Navbar

--- client/index.tsx
+++ client/index.tsx
@@ -4,6 +4,10 @@ import { createRoot } from "react-dom/client";
 
 import { MAny } from "./utils/my-types";
 import SignIn from "./components/Signin";
+import BTCChart  from "./components/Charts/BTC"
+import AAPLChart from "./components/Charts/AAPL"
+import EURUSDChart from "./components/Charts/EURUSD"
+
 
 
 // UI Injection
@@ -14,4 +18,5 @@ const PAINT = (htmlEle: HTMLElement, rEle: MAny) => {
   createRoot(htmlEle).render(rEle);
 };
 
-PAINT(rootHTMLElement, <div> <SignIn /> </div>);
+PAINT(rootHTMLElement, <div> <AAPLChart widgetProps={{"theme": "dark"}} />
+ </div>);
