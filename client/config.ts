061f84b9101c696c04152c0da20eec8911946cf7
Noel Lim <noellimx@gmail.com>
2022-06-27 00:58:43 +0800

    -

---
+++ client/config.ts
@@ -0,0 +1,6 @@
+const config = {
+  env: "production", // or test
+  wsserver: "fruitt.art:8443/ws", // without specify scheme. Example "______:8080/ws" , "mockserver:8080/ws"
+};
+
+export default config;
